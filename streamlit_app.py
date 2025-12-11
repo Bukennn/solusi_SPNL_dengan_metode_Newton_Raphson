80bf1e018c737ada33d390d180562e75da2b965a
Bukennn <akbarmoch322@gmail.com>
2025-12-11 18:36:45 +0700

    Add input fields for second function and initial guesses

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ with col1:
   st.subheader("Input Fungsi dan Parameter")
 
 f1_input = st.text_input("Fungsi f1(x, y):","x**2 + y**2 - 25")
+f2_input = st.text_input("Fungsi f₂(x, y):", "x - y - 1")
+x0 = st.number_input("Tebakan Awal x₀:", value=1.0)
+y0 = st.number_input("Tebakan Awal y₀:", value=1.0)
