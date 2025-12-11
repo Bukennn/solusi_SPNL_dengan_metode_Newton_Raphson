2e03a2d4c3e24af648daf9f710c2cd6a253b2d01
Bukennn <akbarmoch322@gmail.com>
2025-12-11 18:26:23 +0700

    Clean up initial blank lines in streamlit_app.py
    Removed unnecessary blank lines from the start of the file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import numpy as np
-
 st.set_page_config(page_title="Solusi SPNL - Newton Raphson")
-
 st.title("🔢 Solusi SPNL dengan Metode Newton–Raphson")
-
 st.write("Masukkan fungsi dan parameter perhitungan:")
