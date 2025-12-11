e8cac6b2f3438a0e0cd93cd050a47413fb5b5abb
Bukennn <akbarmoch322@gmail.com>
2025-12-11 18:33:51 +0700

    Add input fields for function and parameters

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.set_page_config(page_title="Solusi SPNL - Newton Raphson", layout="wide")
 st.title("Solusi SPNL dengan Metode Newton–Raphson")
 st.write("Masukkan fungsi non-linear dan parameter perhitungannya di bawah ini.")
 
+col1, col2 = st.columns(2)
+with col1:
+  st.subheader("Input Fungsi dan Parameter")
+
+f1_input = st.text_input("Fungsi f1(x, y):","x**2 + y**2 - 25")
