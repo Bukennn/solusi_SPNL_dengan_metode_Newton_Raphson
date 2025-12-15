f69b65caa2f19025e02a1be5739f3e05f9e91bf4
Bukennn <akbarmoch322@gmail.com>
2025-12-15 16:33:26 +0700

    Remove result display from Streamlit app
    Removed result display section from the Streamlit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,19 +56,4 @@ if tombol:
         if error < eps:
             break
 
-    with col2:
-        st.subheader("📌 Hasil Perhitungan")
 
-        st.success(f"""
-        **Solusi Akhir Ditemukan:**
-        - x = `{x}`
-        - y = `{y}`
-        - Error terakhir = `{error}`
-        - Iterasi = `{len(hasil_iterasi)}`
-        """)
-
-        st.subheader("📊 Tabel Iterasi")
-
-        import pandas as pd
-        df = pd.DataFrame(hasil_iterasi, columns=["Iterasi", "x", "y", "Error"])
-        st.dataframe(df, use_container_width=True)
